f492eb1ae9893e3239ba7849f05d7be9e9db6079
fepimentele <fernandapimentelpb@gmail.com>
2026-04-09 19:33:08 -0300

    inicializa o repositório

--- aula04/exemplo01.py
+++ aula04/exemplo01.py
@@ -31,3 +31,42 @@ if usuario == "admin" or senha == "1234":
     print('Login realizado com sucesso')
 else:
     print('Usuário ou senha incorretos.')
+
+# --------------------------------------
+
+# Exemplo IFs encadeados
+    
+nota = 8.9
+if nota >= 9:
+    print('A')
+elif nota >= 7:
+    print('B')
+elif nota >= 5:
+    print('c')
+elif nota >= 3:
+    print('D')
+else:
+    print('E')
+
+# Exemplo de IFs não encadeados
+    
+
+
+
+# IFs aninhados
+nota = float(input('Insira a nota:'))
+frequencia = float(input('Informe a fequência:'))
+
+if nota >= 7:
+    # Aprovado por nota, mas precisa checar a frequência
+
+    if frequencia >= 75:
+        print('Aluno aprovado por Nota e frequência')
+    else:
+        print('Reprovado por frequência baixa')
+else: 
+    print('Reprovado por nota baixa.')
+
+   
+
+
